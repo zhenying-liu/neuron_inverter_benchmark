b72f5d9fbcd1460a80e5663633007d7a4a840bd9
Jan Balewski <janstar1122@gmail.com>
2021-09-17 22:39:02 -0700

    ok

--- torch/toolbox/pm_log_energy_all.sh
+++ torch/toolbox/pm_log_energy_all.sh
@@ -4,9 +4,9 @@ if [[  $NERSC_HOST != perlmutter ]]   ; then
     exit
 fi
 
-# energy cpu_energy memory_energy accel0_energy accel1_energy accel2_energy accel3_energy freshness
+# energy_node cpu_energy memory_energy accel0_energy accel1_energy accel2_energy accel3_energy freshness
 
 appname=$1
 state=$2
 
-echo "ENERGY_LOG: "$(date '+%Y-%m-%dT%H:%M:%S.%N')","`hostname`","`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor`","`cat /sys/cray/pm_counters/energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/cpu_energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/memory_energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/accel0_energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/accel1_energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/accel2_energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/accel3_energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/freshness | cut -d' ' -f1`","$1","$2
+echo "ENERGY_LOG: ,"$(date '+%Y-%m-%dT%H:%M:%S.%N')","`hostname`","`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor`","`cat /sys/cray/pm_counters/energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/cpu_energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/memory_energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/accel0_energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/accel1_energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/accel2_energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/accel3_energy | cut -d' ' -f1`","`cat /sys/cray/pm_counters/freshness | cut -d' ' -f1`","$1","$2
